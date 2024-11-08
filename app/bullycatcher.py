4a70dfa4f4901c1c28f0bf7497914770e3ad3728
lylarosegoddard <118440314+lylarosegoddard@users.noreply.github.com>
2024-11-08 10:01:56 +0000

    Fixing Bullycatcher prompt 2

--- app/bullycatcher.py
+++ app/bullycatcher.py
@@ -8,7 +8,10 @@ class Bullycatcher:
      Return "False" if you dont detect bullying. 
      If you do detect bullying return a detailed explanation of why this is bullying.
      Make sure to only check the last message, the user has already been notified so do not tell them again if they arent still bullying.
-     If the user is not bullying anymore after saying something bullying  ONLY return "False" and DONT return an explanation
+     If the user is not bullying anymore after saying something bullying  ONLY return "False" and DONT return an explanation.
+     Make sure to consider the messages before as the context may rule out the comment being meant in a bullying way. If you sense that the context does rule out the comment as bullying then please ONLY return "False" and DONT return an explanation.
+     Try not to repeat the same message twice.
+     Make sure to understand that after a user has said something bullying you should not tell them again and aim to instead move on from that original comment by returning "False" at other non-bullying comments without giving an explanation.
   """
 
   def __init__(self, name):
